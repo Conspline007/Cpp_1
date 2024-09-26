d72840ef2685fa5b12fc816fb0c5f9e8ef0ec70e
Conspline007 <thamaraikamal07@gmail.com>
2024-09-26 09:31:47 +0530

    Learning one step at a time

---
+++ 01_second.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of n";
+    cin>>n;
+    cout<<"The value of the variable that you entered is"<< n;
+    return 0;
+}
